4756facb890c5fab22a9db8af211f68f014d8c72
ElenaZateeva <zateevaelena14@gmail.com>
2022-11-02 17:59:08 +0300

    Определяет четное ли число OUT

---
+++ Exam002/Program.cs
@@ -0,0 +1,22 @@
+﻿// Принимает на входе число и проверяет на четность. Результат bool
+Console.WriteLine("Введите любое число");
+bool isParsedA = int.TryParse(Console.ReadLine(), out int A);
+
+if (!isParsedA)
+{
+    Console.WriteLine("Ошибка ввода данных");
+    return;
+}
+bool evenNumber = ParityCheck(A);
+
+if (evenNumber)
+    Console.WriteLine($"Значение числа {A} четное. Результат деления {A / 2}.");
+else
+    Console.WriteLine($"Значение числа {A} нечетное.");
+
+bool ParityCheck(int a)
+{
+    bool result = false;
+    if (A % 2 == 0) result = true;
+    return result;
+}
