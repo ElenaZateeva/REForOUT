06cb7748fe4889ca85eae037fb995ff2d85d16f7
ElenaZateeva <zateevaelena14@gmail.com>
2022-11-02 17:11:32 +0300

    REF в методе обмен значений 2х переменных

---
+++ Exam001/Program.cs
@@ -0,0 +1,22 @@
+﻿// Принимаем на входе 2 числа и меняем значения местами (REF)
+//int A=0, B=0;
+Console.WriteLine("Введите любые 2 числа");
+bool isParsedA = int.TryParse(Console.ReadLine(), out int A);
+bool isParsedB = int.TryParse(Console.ReadLine(), out int B);
+
+Console.WriteLine($"Значение чисел: А={A} B={B}");
+
+if (!isParsedA || !isParsedB)
+{
+    Console.WriteLine("Ошибка ввода данных");
+    return;
+}
+DataExchange(ref A, ref B);
+Console.WriteLine($"Значение чисел: А={A} B={B}");
+
+void DataExchange(ref int Aa, ref int Bb)
+{
+    int vari = Aa;
+    Aa = Bb;
+    Bb = vari;
+}
